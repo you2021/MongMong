cb3a54a71ad7c746353a5f49238614641adb610d
pine9 <dbwl232@naver.com>
2021-02-19 23:40:26 +0900

    Login

--- app/src/main/java/com/juj27/mongmong/LoginActivity.java
+++ app/src/main/java/com/juj27/mongmong/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.juj27.mongmong;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,16 +10,33 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.kakao.sdk.auth.LoginClient;
 import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.common.util.Utility;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.User;
 
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 import kotlin.Unit;
 import kotlin.jvm.functions.Function2;
 
 public class LoginActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+    LoginButton fbLoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +46,49 @@ public class LoginActivity extends AppCompatActivity {
         //키 해시값
         String keyHash = Utility.INSTANCE.getKeyHash(this);
         Log.i("KeyHash", keyHash);
+
+        //페이스북
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        fbLoginButton = (LoginButton) findViewById(R.id.login_button);
+       // fbLoginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
+
+        FaceBookLogin();
     }
 
+    void FaceBookLogin(){
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Toast.makeText(LoginActivity.this, "Successful", Toast.LENGTH_SHORT).show(); 
+                    }
+                });
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //카카오톡
     public void clickKakao(View view) {
         LoginClient.getInstance().loginWithKakaoAccount(this, new Function2<OAuthToken, Throwable, Unit>() {
             @Override
